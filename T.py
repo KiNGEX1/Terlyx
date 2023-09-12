c650d2806fb676ad7b0c4baf0264237186418dc0
KiNGEX1 <144549465+KiNGEX1@users.noreply.github.com>
2023-09-12 23:20:59 +0530

    Update T.py

--- T.py
+++ T.py
@@ -1,20 +1,19 @@
 import os
 
 def Google():
-    Search = input(" Search = ")
-    os.system(f"lynx https://google.com/search?q={Search}")
+    Search = input("Search = ")
+    os.system(f"lynx https://www.google.com/search?q={Search}")
 
 def Ecosia():
-    Search = input(" Search =")
-    os.system(f"lynx https://google.com/search?q={Search}")
+    Search = input("Search = ")
+    os.system(f"lynx https://www.ecosia.org/search?q={Search}")
 
 def DuckDuckgo():
-    Search = input(" Search =")
+    Search = input("Search = ")
     os.system(f"lynx https://duckduckgo.com/?q={Search}")
 
 while True:
-    
-    os.systen("pkg install lynx")
+    os.system("pkg install lynx")  # Install lynx (assuming this is required on your system)
     os.system("clear")
     print(" By KiNGEX | https://GitHub.com/KiNGEX1")
     print("__________________________________________")
@@ -31,4 +30,3 @@ while True:
         Ecosia()
     elif choice == '3':
         DuckDuckgo()
-    
